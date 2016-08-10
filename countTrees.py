3aed3fdef03e31736df46cdd7e15c4ef82907466
richardba <richardtrle@gmail.com>
2016-08-10 16:53:18 -0300

    Update countTrees.py

--- countTrees.py
+++ countTrees.py
@@ -6,5 +6,5 @@ def countTrees(nodes, aux):
     for index in range(0,nodes):
       left = aux[index] if len(index+1)>=nodes else countTrees(index)
       right = aux[nodes-index] if len(aux)>=nodes else countTrees(nodes-index)
-      aux[nodes]+= left+right
+      aux[nodes]+= left*right
     return aux[nodes]
